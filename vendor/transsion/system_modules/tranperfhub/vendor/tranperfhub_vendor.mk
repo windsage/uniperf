923379bda8a08b32b155f95b5dbe809d9dbcf3f5
chao.xu5 <chao.xu5@transsion.com>
2025-12-30 09:34:19 +0800

    add makefile Android.bp

---
+++ vendor/transsion/system_modules/tranperfhub/vendor/tranperfhub_vendor.mk
@@ -0,0 +1,47 @@
+# ============================================================
+# TranPerfHub - Vendor 分区配置
+# ============================================================
+
+$(info [TranPerfHub] Loading Vendor partition configuration)
+
+# ==================== Vendor 模块聚合 ====================
+
+PRODUCT_PACKAGES += \
+    tranperfhub_vendor
+
+# ==================== 配置文件复制 ====================
+
+# 复制平台配置文件到 /data/vendor/tranperfhub/
+ifneq ($(TRANPERFHUB_CONFIG_PATH),)
+    PRODUCT_COPY_FILES += \
+        $(TRANPERFHUB_CONFIG_PATH):$(TARGET_COPY_OUT_VENDOR)/etc/tranperfhub/perfhub_params.xml
+
+    $(info [TranPerfHub] Config will be copied to: /vendor/etc/tranperfhub/perfhub_params.xml)
+endif
+
+# ==================== Vendor 属性配置 ====================
+
+# 平台信息
+PRODUCT_VENDOR_PROPERTIES += \
+    vendor.transsion.perfhub.platform=$(TRANPERFHUB_PLATFORM_TYPE) \
+    vendor.transsion.perfhub.chip=$(TRANPERFHUB_CHIP_NAME)
+
+# ==================== SELinux 策略 ====================
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/transsion/system_modules/tranperfhub/sepolicy/vendor
+
+# ==================== Vendor 额外依赖 ====================
+
+# 如果需要额外的 Vendor 模块依赖，在这里添加
+# PRODUCT_PACKAGES += \
+#     some_future_vendor_module
+
+# ==================== 调试配置 ====================
+
+# Debug 版本的额外配置
+ifneq ($(filter eng userdebug,$(TARGET_BUILD_VARIANT)),)
+    PRODUCT_VENDOR_PROPERTIES += \
+        persist.vendor.transsion.perfhub.debug=1 \
+        persist.vendor.transsion.perfhub.log_level=verbose
+endif
