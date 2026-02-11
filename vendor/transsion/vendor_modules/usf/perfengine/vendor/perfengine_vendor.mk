0c7dd64343e9288efaa42f04d89561b409706b69
Jeffrey <jeffery.xchao@gmail.com>
2026-02-11 18:16:46 +0800

    update

--- vendor/transsion/vendor_modules/usf/perfengine/vendor/perfengine_vendor.mk
+++ vendor/transsion/vendor_modules/usf/perfengine/vendor/perfengine_vendor.mk
@@ -2,18 +2,42 @@ PRODUCT_PACKAGES += \
     libperfengine-adapter \
     vendor.transsion.perfengine-V1-ndk
 
-ifneq ($(PERFENGINE_CONFIG_PATH),)
-    PRODUCT_COPY_FILES += \
-        $(PERFENGINE_CONFIG_PATH):$(TARGET_COPY_OUT_VENDOR)/etc/perfengine/perfengine_params.xml
+PERFENGINE_PLATFORM_TYPE := unknown
+PERFENGINE_CHIP_NAME := unknown
+
+ifneq ($(filter msm% kona lahaina taro kalama volcano pineapple sm% sdm%,$(TARGET_BOARD_PLATFORM)),)
+    PERFENGINE_PLATFORM_TYPE := qcom
+    PERFENGINE_CHIP_NAME := $(TARGET_BOARD_PLATFORM)
 endif
 
-ifneq ($(PERFENGINE_MAPPING_PATH),)
-    ifeq ($(PERFENGINE_PLATFORM_TYPE),qcom)
-        PRODUCT_COPY_FILES += \
-            $(PERFENGINE_MAPPING_PATH):$(TARGET_COPY_OUT_VENDOR)/etc/perfengine/platform_mappings_qcom.xml
-    else ifeq ($(PERFENGINE_PLATFORM_TYPE),mtk)
-        PRODUCT_COPY_FILES += \
-            $(PERFENGINE_MAPPING_PATH):$(TARGET_COPY_OUT_VENDOR)/etc/perfengine/platform_mappings_mtk.xml
+ifneq ($(filter mt%,$(TARGET_BOARD_PLATFORM)),)
+    PERFENGINE_PLATFORM_TYPE := mtk
+    PERFENGINE_CHIP_NAME := $(TARGET_BOARD_PLATFORM)
+endif
+
+ifneq ($(filter ums% unisoc%,$(TARGET_BOARD_PLATFORM)),)
+    PERFENGINE_PLATFORM_TYPE := unisoc
+    PERFENGINE_CHIP_NAME := $(TARGET_BOARD_PLATFORM)
+endif
+
+PERFENGINE_BASE_DIR := vendor/transsion/vendor_modules/usf/perfengine
+PERFENGINE_CONFIG_DIR := $(PERFENGINE_BASE_DIR)/vendor/configs
+
+ifeq ($(PERFENGINE_PLATFORM_TYPE),qcom)
+    PERFENGINE_CHIP_CONFIG_DIR := $(PERFENGINE_CONFIG_DIR)/qcom/$(PERFENGINE_CHIP_NAME)
+    PERFENGINE_MAPPING_PATH := $(PERFENGINE_CONFIG_DIR)/platform_mappings_qcom.xml
+else ifeq ($(PERFENGINE_PLATFORM_TYPE),mtk)
+    PERFENGINE_CHIP_CONFIG_DIR := $(PERFENGINE_CONFIG_DIR)/mtk/$(PERFENGINE_CHIP_NAME)
+    PERFENGINE_MAPPING_PATH := $(PERFENGINE_CONFIG_DIR)/platform_mappings_mtk.xml
+else
+    PERFENGINE_CHIP_CONFIG_DIR :=
+    PERFENGINE_MAPPING_PATH :=
+endif
+
+PERFENGINE_CONFIG_PATH :=
+ifneq ($(PERFENGINE_CHIP_CONFIG_DIR),)
+    ifneq ($(wildcard $(PERFENGINE_CHIP_CONFIG_DIR)/perfengine_params.xml),)
+        PERFENGINE_CONFIG_PATH := $(PERFENGINE_CHIP_CONFIG_DIR)/perfengine_params.xml
     endif
 endif
 
