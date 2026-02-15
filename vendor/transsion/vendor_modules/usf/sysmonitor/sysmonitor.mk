7b4acf7d9e4c75abcff80467aaa1ea0e462a21fa
Jeffrey <jeffery.xchao@gmail.com>
2026-02-15 19:09:19 +0800

    update

--- vendor/transsion/vendor_modules/usf/sysmonitor/sysmonitor.mk
+++ vendor/transsion/vendor_modules/usf/sysmonitor/sysmonitor.mk
@@ -1,9 +1,9 @@
 SYSMONITOR_BASE_DIR := vendor/transsion/vendor_modules/usf/sysmonitor
 
 ifneq ($(TARGET_COPY_OUT_SYSTEM),)
-    $(call inherit-product-if-exists, $(SYSMONITOR_BASE_DIR)/system/sysmonitor_system.mk)
+    include $(SYSMONITOR_BASE_DIR)/vendor/sysmonitor_vendor.mk
 endif
 
 ifneq ($(TARGET_COPY_OUT_VENDOR),)
-    $(call inherit-product-if-exists, $(SYSMONITOR_BASE_DIR)/vendor/sysmonitor_vendor.mk)
+    include $(SYSMONITOR_BASE_DIR)/system/sysmonitor_system.mk
 endif
